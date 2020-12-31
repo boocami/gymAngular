499fad95e133b09c289a84a4f4bb110bf7dcaa29
boocami <56263588+boocami@users.noreply.github.com>
2020-12-31 20:26:07 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './Componentes/nav/nav.component';
+import { MainComponent } from './Componentes/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
